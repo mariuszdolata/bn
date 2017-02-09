1dc995aba08c10d068d6c89e0f5d136c9cba2a44
mariusz <mariusz@172.16.17.109>
2017-02-09 10:58:18 +0100

    KRSPobierz
    watki tworzone na nowo po 2h (wczesniej niszczone)

--- src/main/java/crawler/krs_pobierz_pl/profil/KRSPobierzProfilRepository.java
+++ src/main/java/crawler/krs_pobierz_pl/profil/KRSPobierzProfilRepository.java
@@ -38,62 +38,78 @@ public class KRSPobierzProfilRepository extends DatabaseAccess implements Runnab
 	}
 
 	public void run() {
-		// ApplicationContext context = new
-		// AnnotationConfigApplicationContext(DatabaseConfig.class);
-		// KrspobierzBean profilRepository = context.getBean("profilRepository",
-		// KrspobierzBean.class);
-		EntityManager em = this.getEntityManagerFactory().createEntityManager();
-		List<KRSProfilIndex> listToScrape = new ArrayList<KRSProfilIndex>();
 		do {
-			listToScrape.clear();
-			//Pobieranie po 10 elementow
-			TypedQuery<KRSProfilIndex> q = em
-					.createQuery("SELECT i FROM KRSProfilIndex i WHERE status is null order by rand()",
-							KRSProfilIndex.class)
-					.setMaxResults(20);
-			listToScrape = q.getResultList();
-			for(KRSProfilIndex i:listToScrape){
-				logger.info("url="+i.getUrl());
-				try{
-					
-				KRSPobierzProfilGet profilGet = new KRSPobierzProfilGet(this.getThreadId(), this.getProperties(), this.getEntityManagerFactory(), i.getUrl());
-//				KRSPobierzProfilGet profilGet = new KRSPobierzProfilGet(this.getThreadId(), this.getProperties(), this.getEntityManagerFactory(), "http://krs-pobierz.pl/przedsiebiorstwo-wielobranzowe-dabex-i231605");
-				em.getTransaction().begin();
-				i.setStatus(Integer.toString(profilGet.getStatusCode()));
-				em.getTransaction().commit();
-				}catch(Exception e){
-					logger.error("Prawdopodobnie profil nie jest zescrapowany!!!");
-					logger.error(e.getMessage());
-					logger.error(e.getCause());
-					logger.error(e.getLocalizedMessage());
-				}
-				
-				//Opoznienie
-//				Random gen = new Random();
-//				Enumeration keys = this.getProperties().elements();
-//				while(keys.hasMoreElements()){
-//					String key=(String)keys.nextElement();
-//					String value=(String)this.getProperties().getProperty(key);
-//					
-//				}
-//				Integer nextInt = gen.nextInt(Integer.parseInt(this.getProperties().getProperty("randDelayMax"))+Integer.parseInt(this.getProperties().getProperty("delay")));
-//				logger.info("Opoznienie miedzy kolejnymi profilami wynioslo="+nextInt);
-//				long start=System.currentTimeMillis();
-//				try {
-//					Thread.sleep(nextInt);
-//				} catch (InterruptedException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//				long end = System.currentTimeMillis();
-//				logger.info("Czas wykonania="+(end-start));
-				
+			try {
+
+				// ApplicationContext context = new
+				// AnnotationConfigApplicationContext(DatabaseConfig.class);
+				// KrspobierzBean profilRepository =
+				// context.getBean("profilRepository",
+				// KrspobierzBean.class);
+				EntityManager em = this.getEntityManagerFactory().createEntityManager();
+				List<KRSProfilIndex> listToScrape = new ArrayList<KRSProfilIndex>();
+				do {
+					listToScrape.clear();
+					// Pobieranie po 10 elementow
+					TypedQuery<KRSProfilIndex> q = em
+							.createQuery("SELECT i FROM KRSProfilIndex i WHERE status is null order by rand()",
+									KRSProfilIndex.class)
+							.setMaxResults(20);
+					listToScrape = q.getResultList();
+					for (KRSProfilIndex i : listToScrape) {
+						System.gc();
+						logger.info("url=" + i.getUrl());
+						try {
+
+							KRSPobierzProfilGet profilGet = new KRSPobierzProfilGet(this.getThreadId(),
+									this.getProperties(), this.getEntityManagerFactory(), i.getUrl());
+							// KRSPobierzProfilGet profilGet = new
+							// KRSPobierzProfilGet(this.getThreadId(),
+							// this.getProperties(),
+							// this.getEntityManagerFactory(),
+							// "http://krs-pobierz.pl/przedsiebiorstwo-wielobranzowe-dabex-i231605");
+							em.getTransaction().begin();
+							i.setStatus(Integer.toString(profilGet.getStatusCode()));
+							em.getTransaction().commit();
+						} catch (Exception e) {
+							logger.error("Prawdopodobnie profil nie jest zescrapowany!!!");
+							logger.error(e.getMessage());
+							logger.error(e.getCause());
+							logger.error(e.getLocalizedMessage());
+						}
+
+						// Opoznienie
+						// Random gen = new Random();
+						// Enumeration keys = this.getProperties().elements();
+						// while(keys.hasMoreElements()){
+						// String key=(String)keys.nextElement();
+						// String
+						// value=(String)this.getProperties().getProperty(key);
+						//
+						// }
+						// Integer nextInt =
+						// gen.nextInt(Integer.parseInt(this.getProperties().getProperty("randDelayMax"))+Integer.parseInt(this.getProperties().getProperty("delay")));
+						// logger.info("Opoznienie miedzy kolejnymi profilami
+						// wynioslo="+nextInt);
+						// long start=System.currentTimeMillis();
+						// try {
+						// Thread.sleep(nextInt);
+						// } catch (InterruptedException e) {
+						// // TODO Auto-generated catch block
+						// e.printStackTrace();
+						// }
+						// long end = System.currentTimeMillis();
+						// logger.info("Czas wykonania="+(end-start));
+
+					}
+				} while (!listToScrape.isEmpty());
+
+				em.close();
+			} catch (Exception e) {
+				logger.error("Wznowienie watku KRSPobierz Profil Repository");
 			}
-		} while (!listToScrape.isEmpty());
-
-		em.close();
+		} while (!Thread.interrupted());
 		this.getEntityManagerFactory().close();
-
 
 	}
 
--- src/main/java/crawler/krs_pobierz_pl/profil/StartKRSPobierzProfil.java
+++ src/main/java/crawler/krs_pobierz_pl/profil/StartKRSPobierzProfil.java
@@ -1,10 +1,6 @@
 package crawler.krs_pobierz_pl.profil;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Date;
-import java.util.Properties;
 
 import javax.persistence.Persistence;
 
@@ -29,7 +25,7 @@ public class StartKRSPobierzProfil extends StartCrawler {
 			logger.error(e.getMessage());
 			logger.error(e.getClass().getName());
 			e.printStackTrace();
-		} 
+		}
 		if (!properties.getProperty("test_level1").contains("1")) {
 			logger.info("Pominiêto sesjê testow¹");
 			if (properties.getProperty("level1").contains("1")) {
@@ -57,20 +53,33 @@ public class StartKRSPobierzProfil extends StartCrawler {
 
 	}
 
+	@SuppressWarnings("deprecation")
 	private static void startScrapingKRSPobierz_profil() {
 		int threadsNumber = Integer.parseInt(properties.getProperty("numberOfThreads"));
 		int idHost = Integer.parseInt(properties.getProperty("idHost"));
-		KRSPobierzProfilRepository[] repository = new KRSPobierzProfilRepository[threadsNumber];
-		Thread[] threads = new Thread[threadsNumber];
-		for (int i = 0; i < threadsNumber; i++) {
-			repository[i] = new KRSPobierzProfilRepository(i, properties, entityManagerFactory);
-		}
-		for (int i = 0; i < threadsNumber; i++) {
-			threads[i] = new Thread(repository[i]);
-		}
-		for (int i = 0; i < threadsNumber; i++) {
-			threads[i].start();
-		}
+		do {
+			KRSPobierzProfilRepository[] repository = new KRSPobierzProfilRepository[threadsNumber];
+			Thread[] threads = new Thread[threadsNumber];
+			for (int i = 0; i < threadsNumber; i++) {
+				repository[i] = new KRSPobierzProfilRepository(i, properties, entityManagerFactory);
+			}
+			for (int i = 0; i < threadsNumber; i++) {
+				threads[i] = new Thread(repository[i]);
+			}
+			for (int i = 0; i < threadsNumber; i++) {
+				threads[i].start();
+			}
+			try {
+				//zatrzymaj w¹tki po 2h i puœæ od nowa
+				Thread.sleep(7200000);
+				for (int i = 0; i < threadsNumber; i++) {
+					threads[i].stop();
+				}
+			} catch (InterruptedException e) {
+				logger.error("Wznowienie ca³ego procesu");
+				e.printStackTrace();
+			}
+		} while (true);
 
 	}
 
